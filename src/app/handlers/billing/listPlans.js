198ef3cbf34162813f3a130ca12e6ab84771d3cf
Mohd Sufiyan <mosufy@gmail.com>
2018-03-14 03:55:26 +0800

    billingPlans with caching

--- src/app/handlers/billing/listPlans.js
+++ src/app/handlers/billing/listPlans.js
@@ -2,6 +2,9 @@ import stripePackage from 'stripe';
 
 import { response, error } from "../../common";
 import config from '../../../config';
+import cache from '../../cache';
+
+const CACHE_KEY_PLANS = 'cache_key_plans';
 
 export const handler = (event, context, callback) => {
   return listPlans()
@@ -13,9 +16,18 @@ const stripe = stripePackage(config.stripe.SECRET_KEY);
 
 const listPlans = () => {
   return new Promise((resolve, reject) => {
-    return stripe.plans.list({}, (err, plans) => {
-      if (err) reject(err);
-      else resolve(plans);
-    });
+    let plans = cache.get(CACHE_KEY_PLANS);
+
+    if (plans === undefined) {
+      return stripe.plans.list({}, (err, plans) => {
+        if (err) reject(err);
+        else {
+          cache.set(CACHE_KEY_PLANS, plans);
+          resolve(plans);
+        }
+      });
+    } else {
+      return resolve(plans);
+    }
   });
 };
